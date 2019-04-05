adab9389ee71c766c8c5a354ca8dd82851e2c52d
IBRAHIM ELMI <yous-souf93@hotmail.com>
2019-04-05 19:26:16 +0200

    last commit

---
+++ NetBeansProjects/Gestion_Convo/src/gestion_convo/Gestion_Convo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestion_convo;
+
+
+
+/**
+ *
+ * @author joseph chek
+ */
+public class Gestion_Convo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Administrateur A =new Administrateur();
+                A.setSize(927, 556);
+                A.setVisible(true);
+               
+            
+             
+               
+    }
+    
+}
